dabdd17900c3ed046f34ff987e3caa0745d3d90b
lhk0721 <dlgusrb6546@naver.com>
2026-04-01 20:37:14 +0900

    5주차 실습 완료

--- week5/lec05_202010493.cpp
+++ week5/lec05_202010493.cpp
@@ -15,7 +15,7 @@ void BrightnessAdj(BYTE*Img, BYTE*Out, int W, int H, int Val);
 void InverseImg (BYTE*Img, BYTE*Out, int W, int H);
 void outputPrinter(const char* filename, int ImgSize, BYTE* Image, BYTE* Output);
 void ContrastAdj(BYTE*Img, BYTE*Out, int W, int H, double Val);
-void freeer (BYTE* Image, BYTE* Output);
+void freeer (BYTE* Image, BYTE* Output, BYTE* Output_temp, BYTE* Output_temp_2);
 void ObtainHistogram (BYTE*Img,int*Histo,int W, int H);
 void HistogramStretching(BYTE*Img, int* Histo, BYTE*Out, int W, int H);
 void ObtainAHisto(int* Histo, int* AHisto);
@@ -25,9 +25,15 @@ int GonzalezBinThresh(BYTE*Img, BYTE*Out, int*Histo, int W, int H, int tolerance
 void GetMinMax(int*Histo, BYTE*Low, BYTE*High);
 void AverageConv(BYTE*Img, BYTE*Out, int W, int H);
 void GaussAvgConv(BYTE*Img, BYTE*Out, int W, int H);
+void Prewitt_X_Conv(BYTE*Img, BYTE*Output_temp, int W, int H);
+void Prewitt_Y_Conv(BYTE*Img, BYTE*Output_temp, int W, int H);
+void Sobel_X_Conv(BYTE*Img, BYTE*Output_temp, int W, int H);
+void Sobel_Y_Conv(BYTE*Img, BYTE*Output_temp, int W, int H);
+void Laplace_Conv(BYTE*Img, BYTE*Output_temp, int W, int H);
+void Laplace_Conv_DC(BYTE*Img, BYTE*Out, int W, int H);
 
 int main() {
-    fp = fopen("coin.bmp", "rb");
+    fp = fopen("LENNA.bmp", "rb");
 	if(fp == NULL) return -1;
 	fread(&hf, sizeof(BITMAPFILEHEADER), 1, fp);
 	fread(&hInfo, sizeof(BITMAPINFOHEADER), 1, fp);
@@ -36,8 +42,10 @@ int main() {
     int ImgSize = hInfo.biWidth * hInfo.biHeight;
 
     BYTE * Image = (BYTE*)malloc(ImgSize);
-    BYTE * Output = (BYTE*)malloc(ImgSize);
     fread(Image, sizeof(BYTE), ImgSize, fp);
+    BYTE * Output = (BYTE*)malloc(ImgSize);
+    BYTE * Output_temp = (BYTE*)malloc(ImgSize);
+    BYTE * Output_temp_2 = (BYTE*)malloc(ImgSize);
     
     int Histo[256] = {0};
     int AHisto[256] = {0};
@@ -48,21 +56,44 @@ int main() {
     
     if (Thres == -1) {
         printf("Error: threshold calculation failed\n");
-        freeer(Image, Output);
+        freeer(Image, Output,Output_temp,Output_temp_2);
         fclose(fp);
         return -1;
     }
+    
 
-	sameImg(ImgSize, Image, Output);
-    BrightnessAdj(Image,Output,hInfo.biWidth,hInfo.biHeight,70);
-    ContrastAdj(Image,Output,hInfo.biWidth,hInfo.biHeight,1.5);
-    InverseImg (Image, Output, hInfo.biWidth, hInfo.biHeight);
-    HistogramStretching(Image,Histo,Output,hInfo.biWidth,hInfo.biHeight);
-    HistogramEqualization(Image,Histo,AHisto,Output,hInfo.biWidth,hInfo.biHeight);
-    Binarization(Image, Output, hInfo.biWidth, hInfo.biHeight, Thres);
-    AverageConv(Image, Output,hInfo.biWidth,hInfo.biHeight);
-    GaussAvgConv(Image, Output,hInfo.biWidth,hInfo.biHeight);
-    freeer(Image, Output);
+	// sameImg(ImgSize, Image, Output);
+    // BrightnessAdj(Image,Output,hInfo.biWidth,hInfo.biHeight,70);
+    // ContrastAdj(Image,Output,hInfo.biWidth,hInfo.biHeight,1.5);
+    // InverseImg (Image, Output, hInfo.biWidth, hInfo.biHeight);
+    // HistogramStretching(Image,Histo,Output,hInfo.biWidth,hInfo.biHeight);
+    // HistogramEqualization(Image,Histo,AHisto,Output,hInfo.biWidth,hInfo.biHeight);
+    // Binarization(Image, Output, hInfo.biWidth, hInfo.biHeight, Thres);
+    // AverageConv(Image, Output,hInfo.biWidth,hInfo.biHeight);
+    // GaussAvgConv(Image, Output,hInfo.biWidth,hInfo.biHeight);
+
+    // Prewitt_X_Conv(Image, Output_temp,hInfo.biWidth,hInfo.biHeight);
+    // Binarization(Output_temp, Output_temp, hInfo.biWidth, hInfo.biHeight, 50);
+    // Prewitt_Y_Conv(Image, Output_temp_2,hInfo.biWidth,hInfo.biHeight);
+    // Binarization(Output_temp_2, Output_temp_2, hInfo.biWidth, hInfo.biHeight, 51);
+    // for(int i =0; i < ImgSize; i++){
+    //     if(Output_temp[i] > Output_temp_2[i]) Output_temp_2[i] = Output_temp[i];
+    // };
+    // Binarization(Output_temp_2, Output_temp_2, hInfo.biWidth, hInfo.biHeight, 52);
+    
+    // Sobel_X_Conv(Image, Output_temp,hInfo.biWidth,hInfo.biHeight);
+    // Binarization(Output_temp, Output_temp, hInfo.biWidth, hInfo.biHeight, 53);
+    // Sobel_Y_Conv(Image, Output_temp_2,hInfo.biWidth,hInfo.biHeight);
+    // Binarization(Output_temp_2, Output_temp_2, hInfo.biWidth, hInfo.biHeight, 54);
+    // for(int i =0; i < ImgSize; i++){
+    //     if(Output_temp[i] > Output_temp_2[i]) Output_temp_2[i] = Output_temp[i];
+    // };
+    // Binarization(Output_temp_2, Output_temp_2, hInfo.biWidth, hInfo.biHeight, 55);
+    // Laplace_Conv(Image, Output, hInfo.biWidth,hInfo.biHeight);
+    GaussAvgConv(Image, Output_temp,hInfo.biWidth,hInfo.biHeight);
+    Laplace_Conv_DC(Output_temp, Output, hInfo.biWidth,hInfo.biHeight);
+    
+    freeer(Image, Output,Output_temp,Output_temp_2);
 };
 
 void sameImg(int ImgSize, BYTE* Image, BYTE* Output){
@@ -214,22 +245,26 @@ void GetMinMax(int*Histo, BYTE*Low, BYTE*High){
     };
 };
 
-void AverageConv(BYTE*Img, BYTE*Out, int W, int H){ // 박스 평활화
+void AverageConv(BYTE*Img, BYTE*Out, int W, int H){
     int ImgSize = W * H;
-    double Kernel[3][3] = { 
+    double Kernel[3][3] = {
         0.11111,0.11111,0.11111,
         0.11111,0.11111,0.11111,
         0.11111,0.11111,0.11111
     };
+    // 마진(테두리) 픽셀은 원본값 복사
+    // for(int j = 0; j < W; j++) { Out[j] = Img[j]; Out[(H-1)*W+j] = Img[(H-1)*W+j]; }
+    // for(int i = 0; i < H; i++) { Out[i*W] = Img[i*W]; Out[i*W+(W-1)] = Img[i*W+(W-1)]; }
+
     double SumProduct = 0.0;
     for(int i = 1; i < H-1; i++){ //Y좌표(행)
-        for(int j = 0; j < W-1 ; j++){  // x좌표 (열)
+        for(int j = 1; j < W-1 ; j++){  // x좌표 (열)
             for(int m = -1; m <= 1; m ++){ // Kernel 행
                 for(int n = -1; n<= 1; n ++){ // Kernel 열
                     SumProduct += Img[(i+m)*W+(j+n)] * Kernel[m+1][n+1];
                 }
             }
-            Out[i*W + j] = SumProduct;
+            Out[i*W + j] = (BYTE)SumProduct;
             SumProduct = 0.0;
         };
     };
@@ -245,7 +280,7 @@ void GaussAvgConv(BYTE*Img, BYTE*Out, int W, int H){ // 가우시안 평활화
     };
     double SumProduct = 0.0;
     for(int i = 1; i < H-1; i++){ //Y좌표(행)
-        for(int j = 0; j < W-1 ; j++){  // x좌표 (열)
+        for(int j = 1; j < W-1 ; j++){  // x좌표 (열)
             for(int m = -1; m <= 1; m ++){ // Kernel 행
                 for(int n = -1; n<= 1; n ++){ // Kernel 열
                     SumProduct += Img[(i+m)*W+(j+n)] * Kernel[m+1][n+1];
@@ -258,7 +293,7 @@ void GaussAvgConv(BYTE*Img, BYTE*Out, int W, int H){ // 가우시안 평활화
     outputPrinter("GaussAvgConvolutionalized", ImgSize, Img, Out);
 };
 
-void Prewitt_X_Conv(BYTE*Img, BYTE*Out, int W, int H){ // 가우시안 평활화
+void Prewitt_X_Conv(BYTE*Img, BYTE*Output_temp, int W, int H){ 
     int ImgSize = W * H;
     double Kernel[3][3] = { 
         -1.0,0.0,1.0,
@@ -267,18 +302,133 @@ void Prewitt_X_Conv(BYTE*Img, BYTE*Out, int W, int H){ // 가우시안 평활화
     };
     double SumProduct = 0.0;
     for(int i = 1; i < H-1; i++){ //Y좌표(행)
-        for(int j = 0; j < W-1 ; j++){  // x좌표 (열)
+        for(int j = 1; j < W-1 ; j++){  // x좌표 (열)
             for(int m = -1; m <= 1; m ++){ // Kernel 행
                 for(int n = -1; n<= 1; n ++){ // Kernel 열
                     SumProduct += Img[(i+m)*W+(j+n)] * Kernel[m+1][n+1];
                 }
             }
             //0~765 => 0~255
-            Out[i*W + j] = abs((long)SumProduct)/3;
+            Output_temp[i*W + j] = abs((long)SumProduct)/3;
             SumProduct = 0.0;
         };
     };
-    outputPrinter("Prewitt_X_Convolutionalized", ImgSize, Img, Out);
+    outputPrinter("Prewitt_X_Convolutionalized", ImgSize, Img, Output_temp);
+};
+
+void Prewitt_Y_Conv(BYTE*Img, BYTE*Output_temp, int W, int H){ 
+    int ImgSize = W * H;
+    double Kernel[3][3] = { 
+        -1.0,-1.0,-1.0,
+        0.0,0.0,0.0,
+        1.0,1.0,1.0
+    };
+    double SumProduct = 0.0;
+    for(int i = 1; i < H-1; i++){ //Y좌표(행)
+        for(int j = 1; j < W-1 ; j++){  // x좌표 (열)
+            for(int m = -1; m <= 1; m ++){ // Kernel 행
+                for(int n = -1; n<= 1; n ++){ // Kernel 열
+                    SumProduct += Img[(i+m)*W+(j+n)] * Kernel[m+1][n+1];
+                }
+            }
+            //0~765 => 0~255
+            Output_temp[i*W + j] = abs((long)SumProduct)/3;
+            SumProduct = 0.0;
+        };
+    };
+    outputPrinter("Prewitt_Y_Convolutionalized", ImgSize, Img, Output_temp);
+};
+void Sobel_X_Conv(BYTE*Img, BYTE*Output_temp, int W, int H){ 
+    int ImgSize = W * H;
+    double Kernel[3][3] = { 
+        -1.0,0.0,1.0,
+        -2.0,0.0,2.0,
+        -1.0,0.0,1.0
+    };
+    double SumProduct = 0.0;
+    for(int i = 1; i < H-1; i++){ //Y좌표(행)
+        for(int j = 1; j < W-1 ; j++){  // x좌표 (열)
+            for(int m = -1; m <= 1; m ++){ // Kernel 행
+                for(int n = -1; n<= 1; n ++){ // Kernel 열
+                    SumProduct += Img[(i+m)*W+(j+n)] * Kernel[m+1][n+1];
+                }
+            }
+            //0~1020 => 0~255
+            Output_temp[i*W + j] = abs((long)SumProduct)/4;
+            SumProduct = 0.0;
+        };
+    };
+    outputPrinter("Sobel_X_Convolutionalized", ImgSize, Img, Output_temp);
+};
+
+void Sobel_Y_Conv(BYTE*Img, BYTE*Output_temp, int W, int H){ 
+    int ImgSize = W * H;
+    double Kernel[3][3] = { 
+        -1.0,-2.0,-1.0,
+        0.0,0.0,0.0,
+        1.0,2.0,1.0
+    };
+    double SumProduct = 0.0;
+    for(int i = 1; i < H-1; i++){ //Y좌표(행)
+        for(int j = 1; j < W-1 ; j++){  // x좌표 (열)
+            for(int m = -1; m <= 1; m ++){ // Kernel 행
+                for(int n = -1; n<= 1; n ++){ // Kernel 열
+                    SumProduct += Img[(i+m)*W+(j+n)] * Kernel[m+1][n+1];
+                }
+            }
+            //0~1020 => 0~255
+            Output_temp[i*W + j] = abs((long)SumProduct)/4;
+            SumProduct = 0.0;
+        };
+    };
+    outputPrinter("Sobel_Convolutionalized", ImgSize, Img, Output_temp);
+};
+
+void Laplace_Conv(BYTE*Img, BYTE*Out, int W, int H){ 
+    int ImgSize = W * H;
+    double Kernel[3][3] = { 
+        -1.0,-1.0,-1.0,
+        -1.0,8.0,-1.0,
+        -1.0,-1.0,-1.0
+    };
+    double SumProduct = 0.0;
+    for(int i = 1; i < H-1; i++){ //Y좌표(행)
+        for(int j = 1; j < W-1 ; j++){  // x좌표 (열)
+            for(int m = -1; m <= 1; m ++){ // Kernel 행
+                for(int n = -1; n<= 1; n ++){ // Kernel 열
+                    SumProduct += Img[(i+m)*W+(j+n)] * Kernel[m+1][n+1];
+                }
+            }
+            // 0~2040 => 0~255
+            Out[i*W + j] = abs((long)SumProduct)/8;
+            SumProduct = 0.0;
+        };
+    };
+    outputPrinter("Laplace_Convolutionalized", ImgSize, Img, Out);
+};
+void Laplace_Conv_DC(BYTE*Img, BYTE*Out, int W, int H){ 
+    int ImgSize = W * H;
+    double Kernel[3][3] = { 
+        -1.0,-1.0,-1.0,
+        -1.0,9.0,-1.0,
+        -1.0,-1.0,-1.0
+    };
+    double SumProduct = 0.0;
+    for(int i = 1; i < H-1; i++){ //Y좌표(행)
+        for(int j = 1; j < W-1 ; j++){  // x좌표 (열)
+            for(int m = -1; m <= 1; m ++){ // Kernel 행
+                for(int n = -1; n<= 1; n ++){ // Kernel 열
+                    SumProduct += Img[(i+m)*W+(j+n)] * Kernel[m+1][n+1];
+                }
+            }
+            // Out[i*W + j] = abs((long)SumProduct)/8;
+            if(SumProduct > 255.0) Out[i*W + j] = 255;
+            else if(SumProduct < 0.0) Out[i*W + j] = 0;
+            else Out[i*W + j] = (BYTE)SumProduct;
+            SumProduct = 0.0;
+        };
+    };
+    outputPrinter("Laplace_DC_Convolutionalized", ImgSize, Img, Out);
 };
 
 void outputPrinter(const char* filename, int ImgSize, BYTE* Image, BYTE* Output){
@@ -293,7 +443,14 @@ void outputPrinter(const char* filename, int ImgSize, BYTE* Image, BYTE* Output)
 	fclose(fp);
 };
 
-void freeer (BYTE* Image, BYTE* Output){
+void freeer (
+    BYTE* Image, 
+    BYTE* Output,
+    BYTE* Output_temp,
+    BYTE* Output_temp_2
+){
     free(Image);
     free(Output);
+    free(Output_temp);
+    free(Output_temp_2);
 };
